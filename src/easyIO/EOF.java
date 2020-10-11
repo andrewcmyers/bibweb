eff12668411a05b73b85b8cb6e61406af13525d2
Andrew Myers <andru@cs.cornell.edu>
2020-10-10 23:45:45 -0400

    remove own copy of this

--- src/easyIO/EOF.java
+++
@@ -1,6 +0,0 @@
-package easyIO;
-
-/** Represents the end of the input ("end of file") being reached. */
-@SuppressWarnings("serial")
-public class EOF extends Exception {
-}
--- src/easyIO/UnexpectedInput.java
+++
@@ -1,7 +0,0 @@
-package easyIO;
-
-/** Represents encountering input that does not match the expected syntax. */
-@SuppressWarnings("serial")
-public class UnexpectedInput extends Exception {
-
-}
